078f670d9772b77a560bc440589addaac1a731bb
AShaww <71328022+AShaww@users.noreply.github.com>
2023-02-01 12:13:57 +0000

    UserModel, addDbContext

---
+++ CheekyData/CheekyContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CheekyDatads;
+
+public class CheekyContext : DbContext
+{
+    public CheekyContext(DbContextOptions<CheekyContext> options) : base(options)
+    {
+        
+    }
+}
---
+++ CheekyData/Models/Users.cs
@@ -0,0 +1,14 @@
+﻿namespace CheekyDatads.Models;
+
+public class Users
+{
+    public Guid UserId { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public bool Archived { get; set; }
+    public DateTime? LoginDate { get; set; }
+    public DateTime? ArchivedOn { get; set; }
+    public DateTime CreatedOn { get; set; }
+    public DateTime ModifiedOn { get; set; }
+}

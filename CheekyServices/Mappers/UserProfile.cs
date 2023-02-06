1a95847a72cd07e2ebeb9fbd132e345f49f301fe
AShaww <71328022+AShaww@users.noreply.github.com>
2023-02-06 23:30:03 +0000

    Added UserProfile.cs to the getAssemblies

---
+++ CheekyServices/Mappers/UserProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using CheekyData.Models;
+using CheekyModels.Dtos;
+
+namespace CheekyServices.Mappers;
+
+/// <summary>
+/// Mapper for dealing with User data entry 
+/// </summary>
+public class UserProfile : Profile
+{
+    public UserProfile()
+    {
+        CreateMap<User, UserDto>().ReverseMap();
+
+        CreateMap<GoogleUserDto, UserDto>()
+            .ForMember(x => x.FirstName, opt =>
+                opt.MapFrom(x => x.GivenName))
+            .ForMember(x => x.Surname, opt =>
+                opt.MapFrom(x => x.FamilyName));
+    }
+}

006a32e4e8390d0e1bb9cb371c746f2193280c3f
AShaww <71328022+AShaww@users.noreply.github.com>
2023-02-14 20:22:43 +0000

    Implemented ToDoExceptionMessages.cs

---
+++ CheekyServices/Constants/ExceptionMessageConstants/ToDoExceptionMessages.cs
@@ -0,0 +1,7 @@
+﻿namespace CheekyServices.Constants;
+
+public class ToDoExceptionMessages
+{
+    public const string ToDoNotFoundExceptionMessage = "The ToDO with the specified ID does not exist.";
+    public const string ToDoDuplicateExceptionMessage = "The ToDO with the specified ID already exists.";
+}

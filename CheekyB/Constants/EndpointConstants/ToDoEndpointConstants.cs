e3f7ad23be2abec8972518a568ca619a7e6e9947
AShaww <71328022+AShaww@users.noreply.github.com>
2023-02-15 09:37:21 +0000

    Implemented ToDo Endpoint constants

---
+++ CheekyB/Constants/EndpointConstants/ToDoEndpointConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace CheekyB.Constants.EndpointConstants;
+
+public class ToDoEndpointConstants
+{
+    public const string GetToDoByUserIdWithName = "Get ToDo By User Id";
+    public const string InsertToDoWithName = "Create ToDo";
+    public const string UpdateToDoWithName = "Update ToDo";
+}

a01c09818445486ff3c425618ed85cf1dfce0110
AShaww <71328022+AShaww@users.noreply.github.com>
2023-05-11 18:03:41 +0100

    Added AuthEndpointConstants to git

---
+++ CheekyB/Constants/EndpointConstants/AuthEndpointConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace CheekyB.Constants.EndpointConstants
+{
+    public class AuthEndpointConstants
+    {
+        public const string LoginWithName = "Login";
+
+        public const string AuthWithTag = "Auth";
+    }
+}
